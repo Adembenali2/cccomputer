762e00b33ff16b03a0f3862da7006202ec61b5e8
adem2003.benali08@gmail.com <adem2003.benali08@gmail.com>
2025-10-24 15:21:31 +0200

    modification de docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -e
 
-# Railway fournit $PORT au runtime ; on l’applique à Apache
-PORT="${PORT:-8080}"
-
-# Remplace le port par défaut 80 par $PORT
-sed -ri "s/^Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
-sed -ri "s!<VirtualHost \*:80>!<VirtualHost *:${PORT}>!" /etc/apache2/sites-available/000-default.conf
+# Adapter Apache au port Railway si défini
+if [ -n "$PORT" ]; then
+  echo "Listen $PORT" > /etc/apache2/ports.conf
+fi
 
 exec "$@"
-  
-
